52f1414b5c711f6885b5ee51c577cf2b751dccb5
NikolajX4000 <supernikolaj@hotmail.com>
2020-11-05 17:51:25 +0100

    created classes

--- math_set.py
+++ math_set.py
@@ -1,19 +1,19 @@
 class Set:
     def is_member(self, element):
         pass
-    
+
     def intersection(self, other):
         return Intersection(self, other)
-    
+
     def union(self, other):
         return Union(self, other)
-    
+
     def difference(self, other):
         return Difference(self, other)
-    
+
     def compelment(self):
         return Complement(self)
-    
+
     def compare(self, other):
         """
         return -2 if neither is subset of the other
@@ -23,69 +23,76 @@ class Set:
         return 2 if it is undeterminable
         """
         return 2
-    
+
+
 class Intersection(Set):
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+
     def is_member(self, element):
         return self.a.is_member(element) and self.b.is_member(element)
+
 
 class Union(Set):
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+
     def is_member(self, element):
         return self.a.is_member(element) or self.b.is_member(element)
+
 
 class Difference(Set):
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+
     def is_member(self, element):
         return self.a.is_member(element) and not self.b.is_member(element)
+
 
 class Complement(Set):
     def __init__(self, a):
         self.a = a
-    
+
     def is_member(self, element):
         return not self.a.is_member(element)
+
 
 class UniversalSet(Set):
     def is_member(self, element):
         return True
-    
+
     def intersection(self, other):
         return other
-    
+
     def union(self, other):
         return UniversalSet()
-    
+
     def difference(self, other):
         return other.complement
-    
+
     def complement(self):
         return EmptySet()
-    
+
+
 class EmptySet(Set):
     def is_member(self, element):
         return False
-    
+
     def intersection(self, other):
         return EmptySet
-    
+
     def union(self, other):
         return other
-    
+
     def difference(self, other):
         return EmptySet
-    
+
     def complement(self):
         return UniversalSet()
+
 
 class RangeSet(Set):
     def __init__(self, lower, upper):
@@ -94,7 +101,7 @@ class RangeSet(Set):
 
     def is_member(self, element):
         return self.lower <= element <= self.upper
-    
+
     def union(self, other):
         if other is RangeSet and self.upper <= other.lower and self.lower >= self.upper:
             return RangeSet(min(self.lower, other.lower), max(self.upper, other.upper))
---
+++ vssl_parser.py
@@ -0,0 +1,40 @@
+# Define variable
+# DEF X: INTEGER
+# DEF P: BOOLEAN
+
+# Instantiate variable
+# LET X = 100
+# LET P = TRUE
+
+# Set variable to result
+# LET X = Y + 20
+# LET P = X > 20
+
+# Integer operators: +, -, negation
+# Boolean operators: AND, OR, NOT
+# Comparators: <, <=, ==, >=, >
+
+# Blocks: { statements }
+# Selection
+# IF (predicate) block
+# IF (predicate) block ELSE block
+# Iteration: WHILE (predicate) block
+
+class Block:
+    pass
+
+
+class If:
+    pass
+
+
+class While:
+    pass
+
+
+class Def:
+    pass
+
+
+class Let:
+    pass
